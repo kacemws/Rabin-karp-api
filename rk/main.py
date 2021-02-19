6ef43d67df9e8e1bf657fb762070595e1dcfd0f6
kacemws <berras.belkacem@gmail.com>
2021-02-20 00:06:57 +0100

    re organized the structure

---
+++ rk/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rk.scripts import rabin_karp_matcher
+
+from fastapi.middleware.cors import CORSMiddleware
+
+class Message(BaseModel):
+    message: str
+    pattern : str
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post('/find')
+async def findPattern(msg : Message) : 
+    d = 256
+    q = 3
+    x = rabin_karp_matcher(msg.message, msg.pattern, d, q)
+    return {'message' : x}
